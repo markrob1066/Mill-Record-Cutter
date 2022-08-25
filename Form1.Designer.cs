5ef011029c553ec6d44ab3ac27e86aacf5b08063
Mark <Mark@WORK-PC>
2022-08-24 21:11:38 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Mill_Record_Cutter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.file_open = new System.Windows.Forms.Button();
+            this.gen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sample_rate = new System.Windows.Forms.TextBox();
+            this.Dia = new System.Windows.Forms.TextBox();
+            this.speed = new System.Windows.Forms.ComboBox();
+            this.excursion = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // file_open
+            // 
+            this.file_open.Location = new System.Drawing.Point(43, 50);
+            this.file_open.Name = "file_open";
+            this.file_open.Size = new System.Drawing.Size(161, 50);
+            this.file_open.TabIndex = 0;
+            this.file_open.Text = "File Open";
+            this.file_open.UseVisualStyleBackColor = true;
+            this.file_open.Click += new System.EventHandler(this.file_open_Click);
+            // 
+            // gen
+            // 
+            this.gen.Location = new System.Drawing.Point(43, 130);
+            this.gen.Name = "gen";
+            this.gen.Size = new System.Drawing.Size(161, 50);
+            this.gen.TabIndex = 1;
+            this.gen.Text = "Generate G Code";
+            this.gen.UseVisualStyleBackColor = true;
+            this.gen.Click += new System.EventHandler(this.gen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(448, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sample Rate";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(450, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Diameter (in)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(478, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Speed";
+            // 
+            // sample_rate
+            // 
+            this.sample_rate.Location = new System.Drawing.Point(522, 62);
+            this.sample_rate.Name = "sample_rate";
+            this.sample_rate.Size = new System.Drawing.Size(141, 20);
+            this.sample_rate.TabIndex = 5;
+            this.sample_rate.Text = "5000";
+            // 
+            // Dia
+            // 
+            this.Dia.Location = new System.Drawing.Point(522, 109);
+            this.Dia.Name = "Dia";
+            this.Dia.Size = new System.Drawing.Size(141, 20);
+            this.Dia.TabIndex = 6;
+            this.Dia.Text = "3";
+            // 
+            // speed
+            // 
+            this.speed.FormattingEnabled = true;
+            this.speed.Items.AddRange(new object[] {
+            "33",
+            "45",
+            "78"});
+            this.speed.Location = new System.Drawing.Point(522, 159);
+            this.speed.Name = "speed";
+            this.speed.Size = new System.Drawing.Size(141, 21);
+            this.speed.TabIndex = 7;
+            this.speed.SelectedIndexChanged += new System.EventHandler(this.speed_SelectedIndexChanged);
+            // 
+            // excursion
+            // 
+            this.excursion.FormattingEnabled = true;
+            this.excursion.Items.AddRange(new object[] {
+            "33",
+            "45",
+            "78"});
+            this.excursion.Location = new System.Drawing.Point(522, 204);
+            this.excursion.Name = "excursion";
+            this.excursion.Size = new System.Drawing.Size(141, 21);
+            this.excursion.TabIndex = 9;
+            this.excursion.Text = ".006";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(423, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Max Excursion (in)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.excursion);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.speed);
+            this.Controls.Add(this.Dia);
+            this.Controls.Add(this.sample_rate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gen);
+            this.Controls.Add(this.file_open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button file_open;
+        private System.Windows.Forms.Button gen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox sample_rate;
+        private System.Windows.Forms.TextBox Dia;
+        private System.Windows.Forms.ComboBox speed;
+        private System.Windows.Forms.ComboBox excursion;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Mill_Record_Cutter
+{
+    public partial class Form1 : Form
+    {
+
+        Int32 len;
+        Int32 samples;
+        double rpm;
+        double r;
+        double theta;
+
+        double x, y;
+        System.IO.Stream fileInStream;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void file_open_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+            using (OpenFileDialog openInFileDialog = new OpenFileDialog())
+            {
+                openInFileDialog.InitialDirectory = "c:\\";
+                openInFileDialog.Filter = "bin files (*.bin)|*.bin|All files (*.*)|*.*";
+                openInFileDialog.FilterIndex = 1;
+                openInFileDialog.RestoreDirectory = true;
+
+                if (openInFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openInFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    fileInStream = openInFileDialog.OpenFile();
+                    len = (Int32)fileInStream.Length;
+                }
+            }
+            MessageBox.Show(Convert.ToString(len), "File Size", MessageBoxButtons.OK);
+        }
+
+        private void gen_Click(object sender, EventArgs e)
+        {
+            string fileName;
+            string gcode;
+            double data;
+            int d;
+            
+            fileName = openOutFile();
+            if (fileName == "")
+                MessageBox.Show("Could not open file.", "Error", MessageBoxButtons.OK);
+            MessageBox.Show(fileName, "Output File", MessageBoxButtons.OK);
+            var sr = new System.IO.StreamWriter(fileName,false,System.Text.Encoding.ASCII);
+            sr.WriteLine("%");
+            sr.WriteLine("(G20 inch units, G40 remove cutter dia comp, G54 use work coord 1 )");
+            sr.WriteLine("G20 G40 G54");
+            samples = Convert.ToInt32((60.0 / rpm) * Convert.ToDouble(sample_rate.Text) * 1.01);
+            for (Int32 i = 0; i < samples; ++i)
+            {
+
+                theta = ((double)i / (double)samples) * 2 * Math.PI;
+                if (i >= len)
+                    data = 0.0;
+                else
+                {
+                    d = fileInStream.ReadByte();
+                    if (d > 127)  // convert to signed
+                        d -= 256;
+                    data = Convert.ToDouble(d);
+                    data = (data / 128.0) * Convert.ToDouble(excursion.Text);
+                }
+                r = Convert.ToDouble(Dia.Text) / 2.0 + data;
+                y = Math.Sin(theta) * r;
+                x = Math.Cos(theta) * r;
+                if (i == 0)  // rapid move to start thne slow plunge to cutting depth
+                {
+                    gcode = String.Format("G00 F20 X{0:F6} Y{1:F6} Z0.1", x, y);
+                    sr.WriteLine(gcode);
+                    gcode = String.Format("G01 F5 Z-0.01");
+                    sr.WriteLine(gcode);
+                }
+                else
+                {
+                    gcode = String.Format("G01 F5 X{0:F6} Y{1:F6}", x, y);
+                    sr.WriteLine(gcode);
+                }
+             }
+            sr.WriteLine("G00 Z0.25");  // write end of program
+            sr.WriteLine("M02");
+            sr.WriteLine("%");
+            MessageBox.Show("Write Complete", "File Write", MessageBoxButtons.OK);
+            sr.Close();
+        }
+
+        String openOutFile()
+        {
+            // Displays a SaveFileDialog so the user can select the CNC file name 
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "CNC File|*.nc|Text File|*.txt";
+            saveFileDialog1.Title = "Save an CNC File";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName != "")
+                return (saveFileDialog1.FileName);
+            else
+                return ("");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            speed.SelectedIndex = 0; // set to 33 rpm default
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+             
+        }
+
+        private void speed_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (Convert.ToInt32(speed.SelectedItem))
+            {
+                case 33:
+                    rpm = 33.3333333;
+                    break;
+                case 45:
+                    rpm = 45.0;
+                    break;
+                case 78:
+                    rpm = 78.26;
+                    break;
+                default:
+                    rpm = 33.3333333;
+                    break;
+
+            }
+        }
+    }
+}
